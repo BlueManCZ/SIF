10093ffff4a0bc75696d15e9f83019b1510c08c8
BlueManCZ <ivo97@centrum.cz>
2024-03-10 15:20:32 +0100

    Fix xdotool window targeting (#37)

--- fix-wm-class.sh
+++ fix-wm-class.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-
-until xdotool search --name "$1"
-do
-	sleep 1
-done
-
-sleep 1
-
 WM_CLASS="$([ "$2" ] && echo "$2" || echo "$1")"
-xdotool search --name "$1" set_window --classname "$WM_CLASS" --class "$WM_CLASS"
+xdotool search --sync --name "$1" set_window --classname "$WM_CLASS" --class "$WM_CLASS" %@
