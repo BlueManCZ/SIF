c483d7681c45f61b05d1f4b3b2cb00bf80a2e77a
Chapien <accounts@chapien.net>
2025-03-22 13:14:27 -0700

    fix-wm-class.sh now runs while reaper process is running In order to move away from the per game sleep method, I have made it so that fix-wm-class.sh now runs as long as the reaper wrapper process is still running. Should work in 99.9% of cases, tested with UNDERTALE and Pillars.

--- fix-wm-class.sh
+++ fix-wm-class.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 WM_CLASS="$([ "$2" ] && echo "$2" || echo "$1")"
-xdotool search --sync --name "$1" set_window --classname "$WM_CLASS" --class "$WM_CLASS" %@
+reaper_pid=$(pidof reaper)
+while kill -0 $reaper_pid 2> /dev/null; do
+    xdotool search --sync --name "$1" set_window --classname "$WM_CLASS" --class "$WM_CLASS" %@
+    sleep 1
+done
