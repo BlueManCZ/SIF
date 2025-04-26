2ac438628868927f358ed2682b7bb21d56feba69
Chapien <inquiries@chapien.net>
2025-04-26 10:52:56 -0700

    Removed logging.

--- fix-wm-class.sh
+++ fix-wm-class.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-LOG_FILE="/tmp/wmfix.log"
 LAST=${@:$#}
 WM_CLASS="$LAST"
-echo "Starting process: $@" > $LOG_FILE
-echo "Window Class: $WM_CLASS" >> $LOG_FILE
 "$@" &
 PID=$!
 echo "Process ID: $PID" >> $LOG_FILE
 while kill -0 $PID 2> /dev/null; do
-    echo "Process still running: $(date)" >> $LOG_FILE
     xdotool search --sync --name "$LAST" set_window --classname "$WM_CLASS" --class "$WM_CLASS" %@
     sleep 1
 done
-echo "Process ended: $(date)" >> $LOG_FILE
