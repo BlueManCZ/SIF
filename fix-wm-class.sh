fe1b12c9f6568b58cbeb1f1b1565fc53dabdb97e
Chapien <inquiries@chapien.net>
2025-04-26 10:53:37 -0700

    Removed logging, take 2 (I missed one)

--- fix-wm-class.sh
+++ fix-wm-class.sh
@@ -3,7 +3,6 @@ LAST=${@:$#}
 WM_CLASS="$LAST"
 "$@" &
 PID=$!
-echo "Process ID: $PID" >> $LOG_FILE
 while kill -0 $PID 2> /dev/null; do
     xdotool search --sync --name "$LAST" set_window --classname "$WM_CLASS" --class "$WM_CLASS" %@
     sleep 1
